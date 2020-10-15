364dde1d36165edc9fc26dd1a3b66c3888bfea24
God8Head <dc_carlos@live.com>
2020-10-14 20:42:03 -0500

    1

---
+++ CalculadoraEstadística.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+
+//prototipos de funciones
+int MediaAritmetica(int x[]);
+int MediaGeometrica(int x[]);
+int MediaArmonica(int x[]); 
+int Mediana(int x[]);
+int Moda(int x[]);
+int Rango(int x[]);
+int DesviacionEstandar(int x[]);
+int Varianza(int x[]); 
+
+//programa principal
+int main(){
+    //bloque de declarativas
+    int n = 1;
+    int serie[n];
+
+    
+
+
+    return 0;
+}
+
+//definición de funciones
+int MediaAritmetica(int x[]){
+  int a;
+  for () {
+    
+  }
+}
+
+int MediaGeometrica(int x[]){
+
+}
+
+int MediaArmonica(int x[]){
+
+}
+
+int Mediana(int x[]){
+
+}
+
+int Moda(int x[]){
+
+}
+
+int Rango(int x[]){
+
+}
+
+int DesviacionEstandar(int x[]){
+
+}
+
+int Varianza(int x[]){
+
+}
